ee1136f8899ba813c23b061bc79b7e770703190d
tanzeeltariq236 <t.rana231096@gmail.com>
2021-11-21 01:32:39 +0500

    calculator file

--- calculator.py
+++ calculator.py
@@ -1,6 +1,101 @@
-# Menu Input Function-----------------------------------------------
-try:
-    menu = int(input(""" 
+operands = ['+', '-', '*', '/', '//']
+all_operands = ['+', '-', '*', '/', '//', '^', '^^', '%']
+history = {}
+b = 0
+result = None
+
+def print_messages(menu, history, result):
+    if menu == 0:
+        print("Thank you for using Calculator. ")
+    elif menu == 2:
+        if history == {} and result == None :
+            print("No Operation performed uptill now")
+        else:
+            print("Previously Performed Operations :-")
+    elif menu == 3:
+        if history == {}:
+            print("Nothing is available in History")
+        else:
+            print("History is cleared")
+    elif menu == 1:
+        print("Invalid command. Please enter the correct Command")
+
+
+def addition(first, second):
+    return first+second
+
+
+def subtraction(first, second):
+    return first-second
+
+
+def multiplication(first, second):
+    return first*second
+
+
+def division(first, second):
+    return first/second
+
+
+def divison_decimal(first, second):
+    return first//second
+
+
+def power(first, second):
+    if second > 1:
+        first_1 = first * first
+        for _ in range(second-2):
+            first_1 = first_1 * first
+        return first_1
+
+    elif second == 1:
+        return first
+
+    elif second == 0:
+        return 0
+
+    else:
+        return None
+
+
+def under_root(first, second):
+    root = 1/second
+    return first**root
+
+
+def percentage(first, second):
+    return first % second
+
+
+def print_result(result):
+    print(f"Answer is: {result}")
+
+    return first % second
+
+
+def formulate_history(first, second, operand, result):
+    if result!= None:
+        history[b] = [f"{first} {operand} {second} = {result}"]
+        return history[b]
+
+
+def print_history(history):
+    if history != {}:
+        print(history)
+    else:
+        pass
+
+
+def clear_history(history):
+    if history != {}:
+        history.clear()
+        return history
+    else:
+        pass
+
+
+def main_menu():
+    main = int(input(""" 
     ========================MAIN DISPLAY============================
     Press 1 for using Calculator operations : 
     Press 0 for exiting the Calculator :
@@ -8,12 +103,11 @@ try:
     Press 3 for deleting History :
     ================================================================
     """))
-    history = {}
-    b = 0
-    while menu != 0:
-        if menu == 1:
-            # Operand Input Function---------------------------------------------
-            operand = input('''Select the operand you want to implement :
+    return main
+
+
+def operand_function():
+    symbol = input('''Select the operand you want to implement :
         +  for Addition
         -  for Subtraction
         *  for Multiplication
@@ -22,90 +116,72 @@ try:
         ^  for Power
         ^^ for Root Finding
          ''')
+    return symbol
 
-            if operand == '+' or operand == '-' or operand == '*' or operand == '/' or operand == '//':
-                first = int(input("Please enter the first number: "))
-                second = int(input("Please enter the other number: "))
-                if operand == '+':
-                    result = first+second
-                    history[b] = [first, "+", second, "=", result]
-                    print(f"sum is  {result}")
-                elif operand == '-':
-                    result = first-second
-                    history[b] = [first, "-", second, "=", result]
-                    print(f"Difference is  {result}")
-                elif operand == '*':
-                    result = first*second
-                    history[b] = [first, "x", second, "=", result]
-                    print(f"Product is  {result}")
-                elif operand == '/':
-                    result = first/second
-                    history[b] = [first, "/", second, "=", result]
-                    print(f"Divison in Int is  {result}")
-                elif operand == '//':
-                    result = first//second
-                    history[b] = [first, "//", second, "=", result]
-                    print(f"Divison in points is  {result}")
-            elif operand == '%':
-                first = int(input("Please enter the first number: "))
-                second = int(input("Please enter the Percentage : "))
-                result = first % second
-                history[b] = [first, "^", second, "=", result]
-                print(float(f"% of {first} is  {result}"))
+
+def num_input(operand):
+    if operand in operands:
+        first_input = int(input("Please enter the first number: "))
+        second_input = int(input("Please enter the other number: "))
+    elif operand == '^':
+        first_input = int(input("Please enter the number: "))
+        second_input = int(input("Please enter the power: "))
+    elif operand == '^^':
+        first_input = int(input("Please enter the number: "))
+        second_input = int(input("Please enter the root you want to find: "))
+    elif operand == '%':
+        first_input = int(input("Please enter the number: "))
+        second_input = int(input("Please enter the percentage: "))
+    else :
+        first_input=None
+        second_input=None
+    return first_input, second_input
+
+
+try:
+    menu = main_menu()
+    while menu != 0:
+        if menu == 1:
+            operand = operand_function()
+            first, second = num_input(operand)
+            if operand == '+':
+                result = addition(first, second)
+                print_result(result)
+            elif operand == '-':
+                result = subtraction(first, second)
+                print_result(result)
+            elif operand == '*':
+                result = multiplication(first, second)
+                print_result(result)
+            elif operand == '/':
+                result = division(first, second)
+                print_result(result)
+            elif operand == '//':
+                result = divison_decimal(first, second)
+                print_result(result)
             elif operand == '^':
-                first = int(input("Please enter the number: "))
-                second = int(input("Please enter the power: "))
-                if second > 1:
-                    first_1 = first * first  # Power function Code
-                    a = 0
-                    while a != second-2:
-                        first_1 = first_1 * first
-                        a += 1
-                        history[b] = [first, "^", second, "=", first_1]
-                    print(f"Power of {first} is  {first_1}")
-                elif second == 1:
-                    history[b] = [first, "^", second, "=", first]
-                    print(f"Power of {first} is {first}")
-                elif second == 0:
-                    history[b] = [first, "^", second, "=", 0]
-                    print("Power of {first} is 0")
-                else:
-                    print("Cannot give answer to negative powers")
-            elif operand == "^^":
-                first = int(input("Please enter the number: "))
-                second = int(input("Please enter the root you want to find: "))
-                root = 1/second
-                res = first**root
-                history[b] = [first, "root of", second, "=", res]
-                print(f"root of {first} is {res}")
+                result = power(first, second)
+                print_result(result)
+            elif operand == '^^':
+                result = under_root(first, second)
+                print_result(result)
+            elif operand == '%':
+                result = percentage(first, second)
+                print_result(result)
             else:
-                print("Invalid Input")
+                print_messages(menu, history, result)
+            formulate_history(first, second, operand, result)
             b += 1
         elif menu == 2:
-            if history != {}:
-                print("Previous Operations done uptill now: ")
-                print(history)
-            else:
-                print("History is empty already")
+            print_messages(menu, history, result)
+            print_history(history)
         elif menu == 3:
-            if history != {}:
-                clear_history = history.clear()
-                print(f"History is cleared {clear_history}")
-            else:
-                print("Its been empty already")
-        else:
-            print("Invalid Input")
-        menu = int(input(""" 
-    ========================MAIN DISPLAY============================
-    Press 1 for using Calculator operations : 
-    Press 0 for exiting the Calculator :
-    Press 2 for printing Previous Operations :
-    Press 3 for deleting History :
-    ================================================================
-    """))
+            print_messages(menu, history, result)
+            clear_history(history)
+        menu = main_menu()
     if menu == 0:
-        print("Thank you for using calculator")
+        print_messages(menu, history, result)
 except ValueError:
-    print("Invalid Key. Please enter correct command")
+    print("Wrong Input")
 except ZeroDivisionError:
     print("Cant be divided by 0")
